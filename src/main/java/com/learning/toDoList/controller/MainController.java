29378e46ccf75bf036d9805d5eb8064e3701c210
EvgeniyMay <yevgeniy.may@gmail.com>
2020-11-01 09:16:30 +0200

    Init

---
+++ src/main/java/com/learning/toDoList/controller/MainController.java
@@ -0,0 +1,73 @@
+package com.learning.toDoList.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.learning.toDoList.form.ToDoForm;
+import com.learning.toDoList.model.ToDo;
+
+@Controller
+public class MainController {
+	
+	private static List<ToDo> toDoList = new ArrayList<ToDo>();
+	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+	
+	@Value("${error.message}")
+	private String errorMessage;
+	
+	static {
+		toDoList.add(new ToDo("Normal to do", "Create a normal to do list"));
+		toDoList.add(new ToDo("Forget", "Forget about this version"));
+	}
+	
+	@RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("message", welcomeMessage);
+		
+		return "index";
+	}
+	
+	@RequestMapping(value = "/toDoList", method = RequestMethod.GET)
+	public String toDoList(Model model) {
+		model.addAttribute("toDoList", toDoList);
+		
+		return "toDoList";
+	}
+	
+	@RequestMapping(value = "/addToDo", method = RequestMethod.GET)
+	public String addToDo(Model model) {
+		ToDoForm toDoForm = new ToDoForm();
+		model.addAttribute("toDo", toDoForm);
+		
+		return "addToDo";
+	}
+	
+	@RequestMapping(value = "/addToDo", method = RequestMethod.POST)
+	public String saveToDo(Model model,
+			@ModelAttribute("toDoForm")
+			ToDoForm toDoForm) {
+		String title = toDoForm.getTitle();
+		String description = toDoForm.getDescription();
+		
+		if(title != null && title.length() > 0
+			&& description != null && description.length() > 0) {
+			ToDo toDo = new ToDo(title, description);
+			toDoList.add(toDo);
+			
+			return "redirect:toDoList";
+		}
+		
+		model.addAttribute("errorMessage", errorMessage);
+		
+		return "addToDo";
+	}
+}
