f571b0465ca1c4a3ff340c2fbfc2acc29ad06e88
EvgeniyMay <yevgeniy.may@gmail.com>
2020-11-01 16:11:34 +0200

    Now ToDo includes onle text

--- src/main/java/com/learning/toDoList/controller/MainController.java
+++ src/main/java/com/learning/toDoList/controller/MainController.java
@@ -3,7 +3,6 @@ package com.learning.toDoList.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,8 +19,8 @@ public class MainController {
 	private boolean inputError = false;
 	
 	static {
-		toDoList.add(new ToDo("Normal to do", "Create a normal to do list"));
-		toDoList.add(new ToDo("Forget", "Forget about this version"));
+		toDoList.add(new ToDo("Create a normal to do list"));
+		toDoList.add(new ToDo("Forget about this version"));
 	}
 	
 	@RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
@@ -42,12 +41,10 @@ public class MainController {
 	public String saveToDo(Model model,
 			@ModelAttribute("inputToDo")
 			ToDo inputToDo) {
-		String title = inputToDo.getTitle();
-		String description = inputToDo.getDescription();
+		String text = inputToDo.getText();
 		
-		if(title != null && title.length() > 0
-			&& description != null && description.length() > 0) {
-			ToDo toDo = new ToDo(title, description);
+		if(text != null && text.length() > 0) {
+			ToDo toDo = new ToDo(text);
 			toDoList.add(toDo);
 			
 			inputError = false;
--- src/main/java/com/learning/toDoList/model/ToDo.java
+++ src/main/java/com/learning/toDoList/model/ToDo.java
@@ -2,28 +2,22 @@ package com.learning.toDoList.model;
 
 public class ToDo {
 
-	private String title;
-	private String description;
+	private String text;
 	
 	public ToDo() {
 		
 	}
 	
-	public ToDo(String title, String description) {
-		this.title = title;
-		this.description = description;
+	public ToDo(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
 	}
 	
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
 }
